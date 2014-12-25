2391186cde51e3084fcc6c23128659ef1b0b477d
Ambika Gopalan <agopalan@ucsd.edu>
2014-12-25 14:30:19 -0800

    Added imageresults class to handle image urls returned from json objects

--- app/src/main/java/com/example/ambika/webpicsapp/MainActivity.java
+++ app/src/main/java/com/example/ambika/webpicsapp/MainActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText searchQuery;
+    Button searchButton;
+    GridView searchResults;
+    ArrayList<ImageResults> imageResults = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUp();
+
+    }
+
+    //set up UI views
+    public void setUp(){
+        searchQuery = (EditText) findViewById(R.id.editTextQuery);
+        searchButton = (Button) findViewById(R.id.buttonSearch);
+        searchResults = (GridView) findViewById(R.id.gridView);
     }
 
 
